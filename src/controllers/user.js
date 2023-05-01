baed9ebdef54a41b1e3581dafecc9b0113b00a1a
Alex Leandro <alexorcizamus@gmail.com>
2023-05-01 13:24:53 -0300

    :sparkles: feat: Roteamento para leitura de dados

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,6 +3,9 @@ const userService = require('../services/user')
 
 const router = Router()
 
+// ROTAS USUÁRIOS:
+
+// Registrar usuário
 router.post('/', async (req, res) => {
     try {
         const user = await userService.registerUser(req.body)
@@ -12,48 +15,75 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Ler dados do usuário
+router.get('/:userId', async (req, res) => {
+    try {
+        const user = await userService.getUserData(req.params.userId)
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// Renomear usuário
 router.patch('/:userId/rename', async (req, res) => {
     try {
         const user = await userService.renameUser(req.params.userId, req.body.nickname)
-        res.status(201).send(user)
+        res.status(200).send(user)
     } catch (error) {
         res.status(400).send(error)
     }
 })
 
+// Mudar senha do usuário
 router.patch('/:userId/password', async (req, res) => {
     try {
         const user = await userService.changeUserPassword(req.params.userId, req.body.password)
-        res.status(201).send(user)
+        res.status(200).send(user)
     } catch (error) {
         res.status(400).send(error)
     }
 })
 
+// ROTAS VÍCIOS:
+
+// Registrar vício
 router.post('/:userId/vices', async (req, res) => {
     try {
         const user = await userService.addViceToUser(req.params.userId, req.body)
         res.status(201).send(user)
     } catch (error) {
-        res.status(500).send(error)
+        res.status(400).send(error)
     }
 })
 
-router.put('/:userId/vices/:viceId/update', async (req, res) => {
+// Ler vícios de um usuário
+router.get('/:userId/vices', async (req, res) => {
+    try {
+        const user = await userService.getUserVices(req.params.userId)
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// Editar vício
+router.put('/:userId/vices/:viceId', async (req, res) => {
     try {
         const user = await userService.updateVice(req.params.userId, req.params.viceId, req.body)
-        res.status(201).send(user)
+        res.status(200).send(user)
     } catch (error) {
-        res.status(500).send(error)
+        res.status(400).send(error)
     }
 })
 
-router.delete('/:userId/vices/:viceId/delete', async (req, res) => {
+// Deletar vício
+router.delete('/:userId/vices/:viceId', async (req, res) => {
     try {
         const user = await userService.deleteVice(req.params.userId, req.params.viceId)
-        res.status(201).send(user)
+        res.status(200).send(user)
     } catch (error) {
-        res.status(500).send(error)
+        res.status(400).send(error)
     }
 })
 
